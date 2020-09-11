7a54aad88a5da8b3ab914b585f84f5946a556320
Gwon_Dong Lee <dl1820@naver.com>
2020-09-11 12:24:08 +0900

    2020-09-10

--- sliding_window.py
+++ sliding_window.py
@@ -1,7 +1,7 @@
 '''
-작성일 : 2020-09-10
+작성일 : 2020-09-11
 작성자 : 이권동
-코드 개요 : 저장된 버스, 위드라이브 데이터를 불러와 학습, 테스트 데이터셋으로 나누고 학습 후 모델을 저장하는 코드
+코드 개요 : ./data/sorted/ 안에있는 데이터를 window, step 크기만큼 슬라이스 하는 코드
 '''
 import numpy as np
 import os
@@ -15,10 +15,20 @@ filenames = os.listdir("./data/sorted/")
 window = [200]
 step = [20]
 
+
+'''
+    함수 개요 :
+        ./data/sorted/ 안에있는 데이터를 window, step 크기만큼 슬라이스 하는 함수
+    매개변수 :
+        window : 윈도우 크기
+		step : step 크기
+'''
 def data(window, step):
 	pothole_list = []
 	not_pothole_list = []
 	for filename in filenames:
+
+		# 데이터의 형태는 time,x,y,z,latitude,longitude 로 되어있음
 		with open("./data/sorted/" + filename, "r") as f:	
 			l = []
 			l_mode = []
@@ -42,6 +52,7 @@ def data(window, step):
 	print(window, step, p_data.shape, np_data.shape)
 	del p_data, np_data
 
+# 여기서 여러개의 window와 step들로 slice
 for w in window:
 	for s in step:
 		data(w, s)
